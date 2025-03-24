f155bd0e4c9b20079506a298c9ec15396b6bbf5f
Henrique Lima <171154572+henriqk0@users.noreply.github.com>
2025-03-24 09:42:46 -0300

    starting java calculator extreme go horse

---
+++ system-projs/reflection-calculator-extreme-go-horse/src/application/Application.java
@@ -0,0 +1,12 @@
+package application;
+
+import controller.CalcController;
+import model.exception.DivisionByZeroException;
+import model.exception.IndeterminationException;
+import model.exception.InvalidOperationException;
+import view.Menu;
+
+public class Application {
+    public static void main(String[] args) {
+    }
+}
