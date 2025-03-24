c36ecff93666b998f9dd7ac512be6dc10ad9222f
Henrique Lima <171154572+henriqk0@users.noreply.github.com>
2025-03-24 09:33:42 -0300

    Create application/Application.java

---
+++ design-patterns/reflection-fabric-calculator/src/application/Application.java
@@ -0,0 +1,27 @@
+package application;
+
+import controller.CalcController;
+import model.exception.DivisionByZeroException;
+import model.exception.IndeterminationException;
+import model.exception.InvalidOperationException;
+import view.Menu;
+
+public class Application {
+    public static void main(String[] args) {
+        try {
+            Menu menu = new Menu();
+            menu.show();
+
+            CalcController calculator  = new CalcController();
+            System.out.println(calculator.putResult(menu.receiveEntry()));
+        } catch (IndeterminationException e){ 
+            System.out.println("ERRO. A expressão com os operandos resulta em uma interminação matemática." + e.getMessage());
+        } catch (DivisionByZeroException e) {
+            System.out.println("ERRO. Não é possível dividir por 0: " + e.getMessage());
+        } catch (InvalidOperationException e ) {
+            System.out.println("ERRO. Operação solicitada inexistente: " + e.getMessage());
+        } catch (Exception e){
+            System.out.println("ERRO. Ocorreu um problema na execução: " + e.getMessage());
+        }
+    }
+}
